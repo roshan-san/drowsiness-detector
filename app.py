e7f3b5b02f490a34e3e53314b9fd84de326cf9dc
roshan <roshanjeyarubanr@gmail.com>
2025-04-15 13:43:14 -0700

    hjk

--- app.py
+++ app.py
@@ -11,25 +11,26 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
-# UPLOAD_FOLDER = "received_images"
-# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# @app.post("/upload_image/")
-# async def upload_image(file: Annotated[UploadFile, File()]):
-#     if not file:
-#         raise HTTPException(status_code=400, detail="No file uploaded")
 
-#     allowed_extensions = {"png", "jpg", "jpeg"}
-#     file_extension = file.filename.split(".")[-1].lower()
-#     if file_extension not in allowed_extensions:
-#         raise HTTPException(status_code=400, detail="Invalid file format. Allowed formats: png, jpg, jpeg")
+@app.post("/upload_image/")
+async def upload_image(file: Annotated[UploadFile, File()]):
+    UPLOAD_FOLDER = "received_images"
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    if not file:
+        raise HTTPException(status_code=400, detail="No file uploaded")
 
-#     try:
-#         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-#         with open(file_path, "wb") as image_file:
-#             while content := await file.read(1024):  # Read in chunks
-#                 image_file.write(content)
-#         return JSONResponse(content={"filename": file.filename, "message": "Image uploaded successfully"})
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"Error saving the image: {e}")
+    allowed_extensions = {"png", "jpg", "jpeg"}
+    file_extension = file.filename.split(".")[-1].lower()
+    if file_extension not in allowed_extensions:
+        raise HTTPException(status_code=400, detail="Invalid file format. Allowed formats: png, jpg, jpeg")
+
+    try:
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        with open(file_path, "wb") as image_file:
+            while content := await file.read(1024):  # Read in chunks
+                image_file.write(content)
+        return JSONResponse(content={"filename": file.filename, "message": "Image uploaded successfully"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving the image: {e}")
 
